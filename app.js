d2f9c9f860956826910d6b370bd564caf15d71ee
Alexander Middleton <alexander_middleton@charleston.k12.sc.us>
2018-06-04 09:04:21 -0400

    Through section 1

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting App.js')
-
 const fs = require('fs')
 const _ = require('lodash')
 const yargs = require('yargs')
@@ -8,8 +6,6 @@ const notes = require('./notes.js')
 
 const argv = yargs.argv
 var command = process.argv[2]
-console.log('Command: ', command)
-console.log('Yargs', argv)
 
 if (command === 'add') {
   var note = notes.addNote(argv.title, argv.body)
@@ -21,14 +17,30 @@ if (command === 'add') {
   } else {
     console.log('Note title taken')
   }
+
 } else if (command === 'list') {
-  notes.getAll()
+  var allNotes = notes.getAll()
+  if (allNotes) {
+    console.log(`Printing ${allNotes.length} note(s).`)
+    allNotes.forEach(note => notes.logNote(note))
+  }
+
 } else if (command === 'read') {
-  notes.getNote(argv.title)
+  var note = notes.getNote(argv.title)
+  if (note) {
+    console.log('Note found')
+    console.log('--')
+    console.log(`Title: ${note.title}`)
+    console.log(`Body: ${note.body}`)
+  } else {
+    console.log('Note not found')
+  }
+
 } else if (command === 'remove') {
   var noteRemoved = notes.removeNote(argv.title)
   var message = noteRemoved ? 'Note was removed' : 'Note not found'
   console.log(message)
+
 } else {
   console.log('Command not recognized')
 }
--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log('Starting notes.js')
-
 const fs = require('fs')
 
 var fetchNotes = () => {
@@ -31,11 +29,17 @@ var addNote = (title, body) => {
 }
 
 var getAll = () => {
-  console.log('Getting all notes')
+  return fetchNotes()
+  return
 }
 
 var getNote = title => {
-  console.log('Getting note', title)
+  // fetch
+  var notes = fetchNotes()
+  // filter
+  var filteredNotes = notes.filter(note => note.title === title)
+  //return
+  return filteredNotes[0]
 }
 
 var removeNote = title => {
@@ -46,9 +50,16 @@ var removeNote = title => {
   return notes.length !== filteredNotes.length
 }
 
+var logNote = (note) => {
+  console.log('--')
+  console.log(`Title: ${note.title}`)
+  console.log(`Body: ${note.body}`)
+}
+
 module.exports = {
   addNote,
   getAll,
   getNote,
-  removeNote
+  removeNote,
+  logNote
 }
